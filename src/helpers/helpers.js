0be5f72684db47a07e83eaba75a57b3472629815
Akere Adetola <adetolaakere1@gmail.com>
2019-10-03 16:03:34 +0100

    wrote test for functions

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,15 +1,15 @@
-import uuid from 'uuid';
+import uuid from "uuid";
 
 export function sum(...numbers) {
   const enoughArgs = numbers.length > 1;
   if (!enoughArgs) return null;
 
   const somethingNotNumber = numbers.some(number => {
-    return typeof number !== 'number';
+    return typeof number !== "number";
   });
 
   if (somethingNotNumber) {
-    throw new Error('I want numbers');
+    throw new Error("I want numbers");
   }
 
   return numbers.reduce((acc, currentNumber) => {
@@ -18,18 +18,28 @@ export function sum(...numbers) {
 }
 
 export function multiply(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('numbers required');
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("numbers required");
   }
   return a * b;
 }
 
 export function personMaker(name, age) {
+  if (typeof name !== "string" || typeof age !== "number") {
+    throw new Error("Invalid name or age");
+  }
   return {
     id: uuid(),
     name,
-    age,
+    age
   };
 }
 
 // BUILD YOUR OWN UTILITY FUNCTIONS AND TEST THEM OUT!
+
+export function evenNumbers(a, b) {
+  for (let i = a; i <= b; i++) {
+    if (i % 2 === 0) return i;
+    else return i;
+  }
+}
